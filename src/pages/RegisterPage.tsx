0db55ef5da397205b0a20c06cac01711a696df35
FelipeGd02 <felipe.gonzalez.0225@gmail.com>
2026-04-06 17:22:41 -0500

    style:elimnar lineas innecesarias

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, type FormEvent } from 'react'
-import { Link, useNavigate } from 'react-router-dom' // 👈 agregado
+import { Link, useNavigate } from 'react-router-dom' 
 import { AuthCard } from '../components/AuthCard'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
