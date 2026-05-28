54edb7b760e6ec479cca4e36630db197d3f8ec86
FelipeGd02 <felipe.gonzalez.0225@gmail.com>
2026-05-28 00:22:09 -0500

    fix(app): resolve merge conflicts and remove duplicate imports in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,34 @@
 // src/App.tsx
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-
-import './styles/auth.css'
-import './styles/dashboard.css'
-import './styles/projects.css'
-import './styles/profile.css'
-import './styles/calendar.css'
-import './styles/events.css'   // ← new unified events styles
-import './styles/home.css'     // ← new home page styles
-import "./styles/academic.css";
-import "./styles/sports.css";
-
-import { AuthProvider }    from './context/AuthContext'
-import { ProfileProvider } from './context/ProfileContexts'
-import { ProjectProvider } from './context/ProjectContext'
-import { SessionProvider } from './context/SessionContext'
-import { AcademicProvider } from './context/AcademicContext'
-import { SportsProvider }  from './context/SportsContext'
-
-import { ProtectedRoute } from './components/ProtectedRoute'
-
-import { LoginPage }    from './pages/LoginPage'
-import { RegisterPage } from './pages/RegisterPage'
-import { HomePage }     from './pages/HomePage'
-import { AcademicPage } from './pages/AcademicPage'
-import { SportsPage }   from './pages/SportsPage'
-import { ProjectsPage } from './pages/ProjectsPage'
-import { ProfilePage }  from './pages/ProfilePage'
-import { CalendarPage } from './pages/CalendarPage'
-import { SearchPage }   from './pages/SearchPage'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import './styles/auth.css'
-import './styles/dashboard.css'
-import { AcademicPage } from './pages/AcademicPage'
-import { HomePage } from './pages/HomePage'
-import { LoginPage } from './pages/LoginPage'
-import { RegisterPage } from './pages/RegisterPage'
-import { ProfilePage } from './pages/ProfilePage'
-import { SportsPage } from './pages/SportsPage'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/academic" element={<AcademicPage />} />
-        <Route path="/sports" element={<SportsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
-
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import "./styles/auth.css";
 import "./styles/dashboard.css";
 import "./styles/projects.css";
 import "./styles/profile.css";
 import "./styles/calendar.css";
+import "./styles/events.css";
+import "./styles/home.css";
+import "./styles/academic.css";
+import "./styles/sports.css";
 
-
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider }    from "./context/AuthContext";
+import { ProfileProvider } from "./context/ProfileContexts";
 import { ProjectProvider } from "./context/ProjectContext";
 import { SessionProvider } from "./context/SessionContext";
-import { ProfileProvider } from "./context/ProfileContexts";
-
 import { AcademicProvider } from "./context/AcademicContext";
-import { SportsProvider } from "./context/SportsContext";
+import { SportsProvider }  from "./context/SportsContext";
 
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
-import { AcademicPage } from "./pages/AcademicPage";
-import { CalendarPage } from "./pages/CalendarPage";
-import { HomePage } from "./pages/HomePage";
-import { LoginPage } from "./pages/LoginPage";
-import { ProfilePage } from "./pages/ProfilePage";
-import { ProjectsPage } from "./pages/ProjectsPage";
+import { LoginPage }    from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
-import { SearchPage } from "./pages/SearchPage";
-import { SportsPage } from "./pages/SportsPage";
+import { HomePage }     from "./pages/HomePage";
+import { AcademicPage } from "./pages/AcademicPage";
+import { SportsPage }   from "./pages/SportsPage";
+import { ProjectsPage } from "./pages/ProjectsPage";
+import { ProfilePage }  from "./pages/ProfilePage";
+import { CalendarPage } from "./pages/CalendarPage";
+import { SearchPage }   from "./pages/SearchPage";
 
 function App() {
   return (
@@ -99,8 +41,8 @@ function App() {
                 <BrowserRouter>
                   <Routes>
                     {/* Public */}
-                    <Route path="/"        element={<Navigate to="/login" replace />} />
-                    <Route path="/login"   element={<LoginPage />} />
+                    <Route path="/"         element={<Navigate to="/login" replace />} />
+                    <Route path="/login"    element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
 
                     {/* Protected */}
@@ -122,7 +64,7 @@ function App() {
         </ProjectProvider>
       </ProfileProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
